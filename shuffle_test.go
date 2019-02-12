ef5ab8cd380f766acfc508a8dfc4a9b73ff31eaf
protolambda <proto@protolambda.com>
2019-02-13 00:37:01 +0100

    Update benchmarks

--- shuffle_test.go
+++ shuffle_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func BenchmarkPermuteIndex(b *testing.B) {
+func BenchPermuteIndex(listSize uint64, b *testing.B) {
 	hash := sha3.New256()
 	hashFn := func(in []byte) []byte {
 		hash.Reset()
@@ -15,8 +15,6 @@ func BenchmarkPermuteIndex(b *testing.B) {
 
 	// "random" seed for testing. Can be any 32 bytes.
 	seed := [32]byte{123, 42}
-	// list-size to test, test the 4M validator number
-	const listSize = 4000000
 	// rounds of shuffling, constant in spec
 	rounds := uint64(90)
 	// benchmark!
@@ -25,7 +23,7 @@ func BenchmarkPermuteIndex(b *testing.B) {
 	}
 }
 
-func BenchmarkShuffleList(b *testing.B) {
+func BenchIndexComparison(listSize uint64, b *testing.B) {
 	hash := sha3.New256()
 	hashFn := func(in []byte) []byte {
 		hash.Reset()
@@ -35,8 +33,27 @@ func BenchmarkShuffleList(b *testing.B) {
 
 	// "random" seed for testing. Can be any 32 bytes.
 	seed := [32]byte{123, 42}
-	// list to test, test the 4M-sized validator list
-	const listSize = 4000000
+	// rounds of shuffling, constant in spec
+	rounds := uint64(90)
+	// benchmark!
+	for i := 0; i < b.N; i++ {
+		for j := uint64(0); j < listSize; j++ {
+			PermuteIndex(hashFn, rounds, j, listSize, seed)
+		}
+	}
+}
+
+func BenchShuffleList(listSize uint64, b *testing.B) {
+	hash := sha3.New256()
+	hashFn := func(in []byte) []byte {
+		hash.Reset()
+		hash.Write(in)
+		return hash.Sum(nil)
+	}
+
+	// "random" seed for testing. Can be any 32 bytes.
+	seed := [32]byte{123, 42}
+	// list to test
 	testIndices := make([]uint64, listSize, listSize)
 	// fill
 	for i := uint64(0); i < listSize; i++ {
@@ -49,3 +66,14 @@ func BenchmarkShuffleList(b *testing.B) {
 		ShuffleList(hashFn, testIndices, rounds, seed)
 	}
 }
+
+func BenchmarkPermuteIndex4M(b *testing.B) {BenchPermuteIndex(4000000, b)}
+func BenchmarkPermuteIndex40K(b *testing.B) {BenchPermuteIndex(40000, b)}
+func BenchmarkPermuteIndex400(b *testing.B) {BenchPermuteIndex(400, b)}
+//func BenchmarkPermuteIndexComparison4M(b *testing.B) {BenchIndexComparison(4000000, b)}
+func BenchmarkPermuteIndexComparison40K(b *testing.B) {BenchIndexComparison(40000, b)}
+func BenchmarkPermuteIndexComparison400(b *testing.B) {BenchIndexComparison(400, b)}
+func BenchmarkShuffleList4M(b *testing.B) {BenchShuffleList(4000000, b)}
+func BenchmarkShuffleList40K(b *testing.B) {BenchShuffleList(40000, b)}
+func BenchmarkShuffleList400(b *testing.B) {BenchShuffleList(400, b)}
+
